129d870575af291694e9eebfb5ed711accbfd323
hurou927 <god.be.with.ye.fs@gmail.com>
2019-11-24 20:48:30 +0900

    add setenv

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+
+
+export OPENAPI_ROOT=/opt/intel/inteloneapi
+
+source $OPENAPI_ROOT//setvars.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENAPI_ROOT/compiler/2021.1-beta03/linux/compiler/lib/intel64_lin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENAPI_ROOT/compiler/2021.1-beta03/linux/lib/
